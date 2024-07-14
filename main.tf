44d300ce9215e0b6bda82b514a43362b72e88367
Djair Silva <djavasilva@yahoo.com.br>
2024-07-14 14:28:06 -0300

    Terraform ec2

--- main.tf
+++ main.tf
@@ -14,3 +14,16 @@ provider "aws" {
   region  = "us-east-1"
   profile = "bia"
 }
+
+resource "aws_instance" "biaTF" {
+  ami           = "ami-0b72123ee41605393"
+  instance_type = "t3.micro"
+  security_groups = ["sg-0547e898e74a08245","sg-0a145f8bdda4ff32d"]
+  subnet_id = 	"subnet-00869f3ae5c047fa9"
+  tags = {
+      Name = "Bia-terraform-app"
+      App = "BIA"
+      Origem = "Terraform"
+    }
+
+  }
